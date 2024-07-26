ba430aa25fa7edad775ac9b8bf3a0d174a0df5d8
Aashin A K <aashin9605@gmail.com>
2024-07-26 09:42:04 +0530

    create utils

---
+++ Backend/src/models/project.models.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const projectSchema = new Schema(
+    {
+        projectName: {
+            type: String,
+            required: true,
+        },
+        projectAvatar: {
+            type: String,
+            required: true,
+        },
+        tags: {
+            type: [String],
+            required: true,
+        },
+        redirectLink: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+const Project = model("Project", projectSchema);
+export default Project;
